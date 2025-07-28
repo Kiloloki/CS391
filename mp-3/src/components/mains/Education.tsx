47b827d4fa49646b9c77532b1c1fc0022df1bbae
Kiloloki <kiloloki0409@gmail.com>
2025-07-28 14:42:33 -0400

    initial commit

---
+++ mp-3/src/components/mains/Education.tsx
@@ -0,0 +1,24 @@
+export default function Edu() {
+    return (
+        <main id="main-content">
+            <h3 id="main-title">Educational Background</h3>
+            <div>
+                <h5><strong>M.S. in Computer Science</strong>| <em><u>Boston University MA.</u></em></h5>
+                <em>May 2023</em>
+            </div>
+            <div>
+                <h5><strong>Diploma Air/Mixed-Gas Commercial Diving</strong>| <em><u>CDA Technical Institute,
+                    Jacksonville FL.</u></em></h5>
+                <em>May 2016</em>
+            </div>
+            <div>
+                <h5><strong>B.A. in History</strong>| <em><u>California State University, Northridge CA.</u></em></h5>
+                <em>Sep. 2010</em>
+            </div>
+            <div>
+                <h5><strong>A.A. in Economics</strong>| <em><u>Los Angeles Valley College, Los Angeles CA.</u></em></h5>
+                <em>May 2008</em>
+            </div>
+        </main>
+    );
+}
